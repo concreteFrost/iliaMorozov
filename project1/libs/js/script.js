c5fdfa5d7161b6b37535033046918b3f63f8bec6
Ilia Morozov <91851800+concreteFrost@users.noreply.github.com>
2022-02-16 12:14:59 +0000

    fixed php getountryist

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -1,24 +1,25 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
-const OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+const map = L.map("map").setView([51.505, -0.09], 13);
+const OpenStreetMap_Mapnik = L.tileLayer(
+  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }
+).addTo(map);
 
-$(document).ready(()=>{
-    $.ajax(
-        'libs/php/getCountryList.php',{
-            success:function(res){
-                let arr = res.split('!');
-                arr.forEach(el => {
-                    console.log(el)
-                    $("#selectCountry").append(`<option value=${el}>${el}</option>`);
-
-                });
-            
-            },
-            error:function(){
-                console.log('error');
-            }
-        }
-    )
-})
+$(document).ready(() => {
+  $.ajax({
+    url: "libs/php/getCountryList.php",
+    success: function (res) {
+        console.log(res['data'][0]['iso'])
+        res['data'].forEach(el => {
+            $("#selectCountry").append(`<option value=${el['iso']}>
+            ${el['name']}</option>`)
+        });
+    },
+    error: function () {
+      console.log("error");
+    }
+  });
+});
--- project1/libs/php/getCountryList.php
+++ project1/libs/php/getCountryList.php
@@ -1,11 +1,14 @@
 <?php
 
-$executionStartTime = microtime(true);
-
 $country_list_json = file_get_contents('../js/countryBorders.geo.json');
 $decode = json_decode($country_list_json, true);
-foreach($decode['features'] as $res){
-echo $res['properties']['name'].'!';
+$name_iso = [];
+foreach ($decode['features'] as $res) {
+    array_push($name_iso, (object)['name' => $res['properties']['name'], 'iso' => $res['properties']['iso_a2']]);
 }
-
-
+$output["status"]["code"] = "200";
+$output["status"]["name"] = "ok";
+$output["status"]["description"] = "success";
+$output['data'] = $name_iso;
+header('Content-type: application/json; charset=UTF-8');
+echo json_encode($output);
